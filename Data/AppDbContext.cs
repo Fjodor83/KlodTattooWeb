1ec82be3c8f3344b099ed4a944d5f8d21d49ba3e
fjodo <fjodor83>
2025-12-15 01:01:09 +0100

    test

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,10 +9,10 @@ public class AppDbContext : IdentityDbContext, IDataProtectionKeyContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    // Tabelle per Data Protection
+    
     public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
 
-    // Tabelle custom della tua applicazione
+   
     public DbSet<PortfolioItem> PortfolioItems { get; set; }
     public DbSet<BookingRequest> BookingRequests { get; set; }
     public DbSet<TattooStyle> TattooStyles { get; set; }
